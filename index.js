9fc8a37beae1d99fe95b3ec0dc5c5ff79b570f47
mlazzell17@gmail.com <MicheSi>
2020-02-12 13:23:16 -0700

    installed dependencies and added logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require('./server');
+
+server.listen(5000, () => {
+    console.log(`\n** Server is running on 5000 **\n`)
+})
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const timestamp = Date.now()
+  console.log(`${req.method} Request to ${req.originalUrl} at ${timesstamp}`)
+  next();
+}
 
 module.exports = server;
