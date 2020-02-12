f9ac9065ba8845043049a630893a2aa2181e7aec
mlazzell17@gmail.com <MicheSi>
2020-02-12 13:43:48 -0700

    added validateUserId and validateUser middleware

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ server.use(express.json());
 server.use(helmet());
 server.use(logger);
 
+server.use('/api/users', userRouter);
+server.use('api/posts', postRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
@@ -20,6 +23,25 @@ function logger(req, res, next) {
   const timestamp = Date.now()
   console.log(`${req.method} Request to ${req.originalUrl} at ${timesstamp}`)
   next();
+}
+
+function validateUserId(req, res, next) {
+  if(req.id) {
+    req.user = req.id;
+    next();
+  } else {
+    res.status(400).json({Error: res.message})
+  }
+}
+
+function validateUser(req, res, next) {
+  if(req.body) {
+    next()
+  } else if (req.body && !req.body.name) {
+    res.status(400).json({Error: res.message})
+  } else {
+    res.status(400).json({Error: res.message})
+  }
 }
 
 module.exports = server;
